2636c930a1577720d0ac5a07fd81cac790a1f5ed
Nathanael Hauser <nathanaelhauser@gmail.com>
2019-08-28 11:54:36 -0700

    Moved const arrays to separate file

---
+++ assets/javascript/constants.js
@@ -0,0 +1,54 @@
+const states = ['ALABAMA', 'ALASKA', 'ARIZONA', 'ARKANSAS', 'CALIFORNIA', 'COLORADO', 'CONNECTICUT', 'DELAWARE', 'FLORIDA', 'GEORGIA', 'HAWAII', 'IDAHO', 'ILLINOIS', 'INDIANA', 'IOWA', 'KANSAS', 'KENTUCKY', 'LOUISIANA', 'MAINE', 'MARYLAND', 'MASSACHUSETTS', 'MICHIGAN', 'MINNESOTA', 'MISSISSIPPI', 'MISSOURI', 'MONTANA', 'NEBRASKA', 'NEVADA', 'NEW HAMPSHIRE', 'NEW JERSEY', 'NEW MEXICO', 'NEW YORK', 'NORTH CAROLINA', 'NORTH DAKOTA', 'OHIO', 'OKLAHOMA', 'OREGON', 'PENNSYLVANIA', 'RHODE ISLAND', 'SOUTH CAROLINA', 'SOUTH DAKOTA', 'TENNESSEE', 'TEXAS', 'UTAH', 'VERMONT', 'VIRGINIA', 'WASHINGTON', 'WEST VIRGINIA', 'WISCONSIN', 'WYOMING']
+
+const videoCodes = [
+    'mpg27rsxsLM', // Alabama
+    'jQaWnTa4xMg', // Alaska
+    'P1Zicw4n0rE', // Arizona
+    'Kk8nBm1QlVQ', // Arkansas
+    'YrnS5WQY1ro', // California
+    'nH_QgFT2WQw', // Colorado
+    'lq0TDUEFAEM', // Connecticut
+    'zBcDOFq3ypo', // Delaware
+    'pHBnjBvbPqw', // Florida
+    'KaC3IH3BzWI', // Georgia
+    'fkT1FFF1Y9M', // Hawaii
+    'c7uirDmpuKQ', // Idaho
+    'IfUp0NuwAKU', // Illinois
+    'gCCbCZhOl3U', // Indiana
+    'NqTp3iXkZBg', // Iowa
+    '4hF5mguEw4M', // Kansas
+    'vbGG5R4tXoY', // Kentucky
+    'akpo1zofy0w', // Louisiana
+    '_qebQVX3sLM', // Maine
+    '2Lac5s-QkI0', // Maryland
+    'OlD4cQuKsrQ', // Massachusetts
+    'AVHw2Ga4mow', // Michigan
+    'o3sRoXpfOjE', // Minnesota
+    'PRlfr4wirAE', // Mississippi
+    '_s7ZsohKv7Q', // Missouri
+    'sT4kmLetIoM', // Montana
+    'zMTxZnbtLKw', // Nebraska
+    'EoW0YusZB58', // Nevada
+    'FIohB1PmvfM', // New Hampshire
+    '3FfRTwBdRBY', // New Jersey
+    'RbARhECVZLQ', // New Mexico
+    's0MVyYPWJCA', // New York
+    'V2lZKhx9J1s', // North Carolina
+    'wsJ2mG3y_sk', // North Dakota
+    '3mChT55f_rQ', // Ohio
+    'aK2nQBdVxPE', // Oklahoma
+    'pEiD7_LSAQM', // Oregon
+    'U6ekJh7QG74', // Pennsylvania
+    'GOvyC-BaOHA', // Rhode Island
+    'KoXAqh-X8PQ', // South Carolina
+    'Ghx1GJy1VCU', // South Dakota
+    'olL-bDGphmM', // Tennessee
+    'UFUuh_XNXIU', // Texas
+    'ExQVrbucG2g', // Utah
+    'zFTRYLS_93s', // Vermont
+    'siSNj-PMCS8', // Virginia
+    'QaM_h28Wr0g', // Washington
+    'GXLUpDFUxIo', // West Virginia
+    'BQyh-Ct-2LU', // Wisconsin
+    'vdcyjTGVglo'  // Wyoming
+]
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,107 +1,3 @@
-const states = ['ALABAMA', 'ALASKA', 'ARIZONA', 'ARKANSAS', 'CALIFORNIA', 'COLORADO', 'CONNECTICUT', 'DELAWARE', 'FLORIDA', 'GEORGIA', 'HAWAII', 'IDAHO', 'ILLINOIS', 'INDIANA', 'IOWA', 'KANSAS', 'KENTUCKY', 'LOUISIANA', 'MAINE', 'MARYLAND', 'MASSACHUSETTS', 'MICHIGAN', 'MINNESOTA', 'MISSISSIPPI', 'MISSOURI', 'MONTANA', 'NEBRASKA', 'NEVADA', 'NEW HAMPSHIRE', 'NEW JERSEY', 'NEW MEXICO', 'NEW YORK', 'NORTH CAROLINA', 'NORTH DAKOTA', 'OHIO', 'OKLAHOMA', 'OREGON', 'PENNSYLVANIA', 'RHODE ISLAND', 'SOUTH CAROLINA', 'SOUTH DAKOTA', 'TENNESSEE', 'TEXAS', 'UTAH', 'VERMONT', 'VIRGINIA', 'WASHINGTON', 'WEST VIRGINIA', 'WISCONSIN', 'WYOMING']
-
-const videos = [
-    // Alabama
-    'https://www.youtube.com/embed/mpg27rsxsLM?controls=0', 
-    // Alaska
-    'https://www.youtube.com/embed/jQaWnTa4xMg?controls=0',
-    // Arizona
-    'https://www.youtube.com/embed/jQaWnTa4xMg?controls=0',
-    // Arkansas
-    'https://www.youtube.com/embed/Kk8nBm1QlVQ?controls=0',
-    // California
-    'https://www.youtube.com/embed/YrnS5WQY1ro?controls=0',
-    // Colorado
-    'https://www.youtube.com/embed/nH_QgFT2WQw?controls=0',
-    // Connecticut
-    'https://www.youtube.com/embed/lq0TDUEFAEM?controls=0',
-    // Delaware
-    'https://www.youtube.com/embed/zBcDOFq3ypo?controls=0',
-    // Florida
-    'https://www.youtube.com/embed/pHBnjBvbPqw?controls=0',
-    // Georgia
-    'https://www.youtube.com/embed/KaC3IH3BzWI?controls=0',
-    // Hawaii
-    'https://www.youtube.com/embed/fkT1FFF1Y9M?controls=0',
-    // Idaho
-    'https://www.youtube.com/embed/c7uirDmpuKQ?controls=0',
-    // Illinois
-    'https://www.youtube.com/embed/IfUp0NuwAKU?controls=0',
-    // Indiana
-    'https://www.youtube.com/embed/gCCbCZhOl3U?controls=0',
-    // Iowa
-    'https://www.youtube.com/embed/NqTp3iXkZBg?controls=0',
-    // Kansas
-    'https://www.youtube.com/embed/4hF5mguEw4M?controls=0',
-    // Kentucky
-    'https://www.youtube.com/embed/vbGG5R4tXoY?controls=0',
-    // Louisiana
-    'https://www.youtube.com/embed/akpo1zofy0w?controls=0',
-    // Maine
-    'https://www.youtube.com/embed/_qebQVX3sLM?controls=0',
-    // Maryland
-    'https://www.youtube.com/embed/2Lac5s-QkI0?controls=0',
-    // Massachusetts
-    'https://www.youtube.com/embed/OlD4cQuKsrQ?controls=0',
-    // Michigan
-    'https://www.youtube.com/embed/AVHw2Ga4mow?controls=0',
-    // Minnesota
-    'https://www.youtube.com/embed/o3sRoXpfOjE?controls=0',
-    // Mississippi
-    'https://www.youtube.com/embed/PRlfr4wirAE?controls=0',
-    // Missouri
-    'https://www.youtube.com/embed/_s7ZsohKv7Q?controls=0',
-    // Montana
-    'https://www.youtube.com/embed/sT4kmLetIoM?controls=0',
-    // Nebraska
-    'https://www.youtube.com/embed/zMTxZnbtLKw?controls=0',
-    // Nevada
-    'https://www.youtube.com/embed/EoW0YusZB58?controls=0',
-    // New Hampshire
-    'https://www.youtube.com/embed/FIohB1PmvfM?controls=0',
-    // New Jersey
-    'https://www.youtube.com/embed/3FfRTwBdRBY?controls=0',
-    // New Mexico
-    'https://www.youtube.com/embed/RbARhECVZLQ?controls=0',
-    // New York
-    'https://www.youtube.com/embed/s0MVyYPWJCA?controls=0',
-    // North Carolina
-    'https://www.youtube.com/embed/V2lZKhx9J1s?controls=0',
-    // North Dakota
-    'https://www.youtube.com/embed/wsJ2mG3y_sk?controls=0',
-    // Ohio
-    'https://www.youtube.com/embed/3mChT55f_rQ?controls=0',
-    // Oklahoma
-    'https://www.youtube.com/embed/aK2nQBdVxPE?controls=0',
-    // Oregon
-    'https://www.youtube.com/embed/pEiD7_LSAQM?controls=0',
-    // Pennsylvania
-    'https://www.youtube.com/embed/U6ekJh7QG74?controls=0',
-    // Rhode Island
-    'https://www.youtube.com/embed/GOvyC-BaOHA?controls=0',
-    // South Carolina
-    'https://www.youtube.com/embed/KoXAqh-X8PQ?controls=0',
-    // South Dakota
-    'https://www.youtube.com/embed/Ghx1GJy1VCU?controls=0',
-    // Tennessee
-    'https://www.youtube.com/embed/olL-bDGphmM?controls=0',
-    // Texas
-    'https://www.youtube.com/embed/UFUuh_XNXIU?controls=0',
-    // Utah
-    'https://www.youtube.com/embed/ExQVrbucG2g?controls=0',
-    // Vermont
-    'https://www.youtube.com/embed/zFTRYLS_93s?controls=0',
-    // Virginia
-    'https://www.youtube.com/embed/siSNj-PMCS8?controls=0',
-    // Washington
-    'https://www.youtube.com/embed/QaM_h28Wr0g?controls=0',
-    // West Virginia
-    'https://www.youtube.com/embed/GXLUpDFUxIo?controls=0',
-    // Wisconsin
-    'https://www.youtube.com/embed/BQyh-Ct-2LU?controls=0',
-    // Wyoming
-    'https://www.youtube.com/embed/vdcyjTGVglo?controls=0']
-
 console.log(document.getElementById('video'))
 let iframe = document.getElementById('video')
 iframe.style.display = 'none'
@@ -156,9 +52,10 @@ game.resetGame()
 game.updateDocument()
 
 document.onkeypress = event => {
-
+    // Check if input is letter
     if (97 <= event.keyCode && event.keyCode <= 122) {
         let key = event.key.toUpperCase()
+        // Check for redundant guesses
         if (!game.currentGuesses.includes(key)) {
             game.currentGuesses += `${key} `
 
@@ -175,8 +72,7 @@ document.onkeypress = event => {
                 if (!game.displayState.includes('_')) {
                     game.wins++
                     alert('You won!!')
-                    console.log(videos[game.chosenState])
-                    iframe.setAttribute('src', videos[game.chosenState])
+                    iframe.setAttribute('src', `https://www.youtube.com/embed/${videoCodes[game.chosenState]}?controls=0`)
                     iframe.style.display = 'block'
                     // game.resetGame()
                 }
